442d1721c7ff3f0dd135d6c8b0d21e5142597c82
Pierre Muller <pierremanu.muller@gmail.com>
2020-04-01 15:59:37 +0200

    contact page edit : addaed a second feedback depending on the subject chosen

--- _form.php
+++ _form.php
@@ -1,29 +1,41 @@
 <h2>Send me a message !</h2>
 <form action="contact.php" method="post" name="formSaise" class="form">
+    <!-- First name -->
     <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" name="name" required="" class="form-control" id="name" placeholder="MrBean" pattern="[A-Z][A-Za-z' -]+">
+        <label for="firstname">First name</label>
+        <input type="text" name="firstname" required="" class="form-control" id="firstname" placeholder="John" pattern="[A-Z][A-Za-z' -]+">
     </div>
+    <!-- Last name -->
+    <div class="form-group">
+        <label for="lastname">Name</label>
+        <input type="text" name="lastname" required="" class="form-control" id="lastname" placeholder="Doe" pattern="[A-Z][A-Za-z' -]+">
+    </div>
+    <!-- E-mail -->
     <div class="form-group">
         <label for="mail">Email address</label>
         <input type="email" name="mail" required="" class="form-control" id="mail" placeholder="name@example.com">
     </div>
+    <!-- Tel -->
     <div class="form-group">
         <label for="tel">Phone number </label>
         <input type="tel" name="tel" required class="form-control" id="tel" placeholder="0123456789" minlength="10" maxlength="10" pattern="[0-9]{10}">
     </div>
+    <!-- Subject -->
     <div class="form-group">
         <label for="subject">Subject</label>
         <select name="subject" id="subject" required="" class="form-control">
-            <option class="form-option" selected> - Select a subject - </option>
-            <option class="form-option">I want to hire you</option>
-            <option class="form-option">I want to meet you</option>
+            <option class="form-option" value="nothing interesting" selected> - Select a subject - </option>
+            <option class="form-option" value="hireing me">I want to hire you</option>
+            <option class="form-option" value="a possible meeting">I want to meet you</option>
+            <option class="form-option" value="riding my car">I want to ride your car</option>
         </select>
     </div>
+    <!-- Message -->
     <div class="form-group">
         <label for="msg">Message</label>
         <textarea required="" name="msg" class="form-control" id="msg" rows="8"></textarea>
     </div>
+    <!-- Button -->
     <div class="button">
         <button type="submit" name="send-message" value="OK">Send</button>
     </div>
--- formFeedback.php
+++ formFeedback.php
@@ -1,16 +1,41 @@
 <?php
 $formData = [];
 $formData = [
-    'name' => htmlspecialchars($_POST['name']),
+    'firstname' => htmlspecialchars($_POST['firstname']),
+    'lastname' => htmlspecialchars($_POST['lastname']),
     'email' => htmlspecialchars($_POST['mail']),
     'tel' => htmlspecialchars($_POST['tel']),
     'subject' => htmlspecialchars($_POST['subject']),
     'msg' => htmlspecialchars($_POST['msg'])
 ];
 ?>
+<?php
 
-<div class="form-feedback">
-    <p>Thank you <strong><?= $formData['name']; ?></strong> for reaching to me about <strong><?= $formData['subject']; ?></strong> !</p>
-    <p>I will come back to you as soon as possible by mail at the address <strong><?= $formData['email']; ?></strong>, or I will give you a call at <strong><?= $formData['tel']; ?></strong>.</p>
-    <p>Greetings, and see you soon !</p>
-</div>
+// Two types of feedback depending on the subject chosen by the user
+if ($_POST['subject'] != 'riding my car') {
+
+?>
+    <div class="form-feedback">
+        <p>Thank you <strong><?= $formData['firstname'] . ' ' . $formData['lastname']; ?></strong> for reaching to me
+            about <strong><?= $formData['subject']; ?></strong> !</p>
+        <p>I will come back to you as soon as possible by mail at the address
+            <strong><?= $formData['email']; ?></strong>, or I will give you a call at
+            <strong><?= $formData['tel']; ?></strong>.</p>
+        <p>Greetings, and see you soon !</p>
+    </div>
+<?php
+
+}
+else {
+
+?>
+    <div class="form-feedback">
+        <p>Thank you <strong><?= $formData['firstname'] . ' ' . $formData['lastname']; ?></strong> for reaching to me
+            about <strong><?= $formData['subject']; ?></strong> !</p>
+        <p>Bloody hell, you can forget about it, go away freak !</p>
+        <p>Greetings, and see you soon !</p>
+    </div>
+<?php
+
+}
+
